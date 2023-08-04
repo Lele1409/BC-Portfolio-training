fc130e8aaece2f471117512283ab13a3dd0a94bb
Lele1409 <leander.decone@gmail.com>
2023-08-04 13:44:30 +0200

    added img-caroussel

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+/* UTIL FUNCTIONS */
+function insertAfter(referenceNode, newNode) {
+    referenceNode.parentNode.insertBefore(newNode, referenceNode.nextSibling);
+}
+
+
 /* Set footer navbar contents to replicate the header's content */
 // Get a NodeList of all the buttons in the navbar
 const headerNavbarButtons = document.querySelectorAll("#header-navbar > .navbar-button")
@@ -11,8 +17,9 @@ headerNavbarButtons.forEach(function(button) {
 })
 
 
-/* When the window gets resized: Place circles on the timeline-line at the same height as the sections title */
-window.addEventListener("resize", placeCircles)
+/* When the window gets resized or when after a given interval: Place circles on the timeline-line at the same height as the sections title */
+window.onresize = placeCircles()
+window.setInterval(placeCircles, 2000)
 
 function placeCircles() {
     // Delete all exisiting circles
@@ -54,24 +61,39 @@ function placeCircles() {
     }
 }
 
-// Run once at start
-placeCircles()
 
-
-/* Format a caroussel to contain */
+/* Format a caroussel to contain the images */
 const caroussels = document.querySelectorAll("img-caroussel")
 caroussels.forEach( function(caroussel) {
-    // only run if caroussel element has more than one image
+    // Only run if caroussel element has more than one image
     if (caroussel.children.length <= 1) {
         return
     }
-    // if there is more than one image: run initial setup
+
+    // Add a text element below the image, showing the progress through the caroussel
+    const progressCounter = document.createElement('caroussel-progress-tracker')
+    progressCounter.innerHTML = `1 / ${caroussel.children.length}`
+    caroussel.appendChild(progressCounter)
+
+    // Add buttons to the sides of this text
+    arrows = ['←', '→']
+    for (let i = 0; i < 2; i++) {
+        let carousselButton = document.createElement("button")
+        carousselButton.setAttribute('direction', i)
+        carousselButton.textContent = arrows[i]
+        carousselButton.addEventListener("click", function() {
+            carousselButtonOnClick(carousselButton)
+        })
+        progressCounter.appendChild(carousselButton)
+    }
+
+    // If there is more than one image: run initial setup
     // give continous ids to the images
-    let i = 0
+    let i = 1
     caroussel.setAttribute("current-img", i)
     for (let img of caroussel.children) {
-        img.id = i
-        if (i > 0) {
+        img.setAttribute("img-order", i)
+        if (i > 1 && !(img === caroussel.lastChild)) {
             img.style.height = 0
             img.style.width = 0
         }
@@ -79,3 +101,34 @@ caroussels.forEach( function(caroussel) {
     }
 })
 
+function carousselButtonOnClick(carousselButton) {
+    // Depending on which button has been clicked we determine the direction in which the caroussel will turn
+    let i = 0
+    if (carousselButton.getAttribute('direction') === '1') { //right
+        i = 1
+    } else if (carousselButton.getAttribute('direction') === '0') { // left
+        i = -1
+    }
+
+    // Change displayed image
+    let imgCaroussel = carousselButton.parentNode.parentNode
+
+    let currentImage = Number(imgCaroussel.getAttribute("current-img"))
+    let newCurrentImage = currentImage + i
+    let carousselLength = imgCaroussel.children.length - 1  // Since the progresstracker element does not count
+    
+    // Only change images if they are in the range of existing images
+    if (0 < newCurrentImage && newCurrentImage <= carousselLength && !(currentImage === newCurrentImage)) {
+        imgCaroussel.setAttribute("current-img", newCurrentImage)
+        carousselButton.parentNode.childNodes[0].textContent = `${newCurrentImage} / ${carousselLength}`
+
+        imgCaroussel.querySelectorAll("img").forEach( function(img) {
+            if (img.getAttribute("img-order") == currentImage) {  // Disable old image
+                img.style.height = '0px'
+                img.style.width = '0px'
+            } else if (img.getAttribute("img-order") == newCurrentImage) {  // Enable new image
+                img.style = ''
+            }
+        })
+    }
+}
